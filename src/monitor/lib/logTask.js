c2bc0e50f3a35089cbb7e5e2e5f19a58f3640c00
李承斌 <1307437149@qq.com>
2022-07-01 09:44:17 +0800

    完成了前端事件卡顿监控

--- src/monitor/lib/logTask.js
+++ src/monitor/lib/logTask.js
@@ -1,24 +1,27 @@
 import getLastEvent from "../utils/getLastEvent"
 import getSelector from "../utils/getSelector"
+import formatTime from "../utils/formatTime"
+
 export default function longTask(){
   new PerformanceObserver((list)=>{
-    console.log(list)
-    // list.getEntries().forEach(entry => {
-    //     if (entry.duration > 100) {
-    //         let lastEvent = getLastEvent();
-    //         requestIdleCallback(() => {
-    //           let log = {
-    //             kind: 'experience',
-    //             type: 'longTask',
-    //             eventType: lastEvent.type,
-    //             startTime: entry.startTime,// 开始时间
-    //             duration: entry.duration,// 持续时间
-    //             selector: lastEvent ? getSelector(lastEvent.path || lastEvent.target) : ''
-    //           }
-    //           console.log("前端卡顿时间",log)
-    //         });
-    //     }
-    // })
+    list.getEntries().forEach((item)=>{
+      // 判断如果改事件执行超过200毫秒,则收集日志
+      if(item.duration > 200){
+        let lastEvent = getLastEvent();
+        // 在浏览器空闲的时候被调用,他不遵循先进先调用的顺序执行
+        requestIdleCallback(()=>{
+          let log = {
+            kind:"experience",
+            type: 'longTask',
+            eventType:lastEvent.type,
+            startTime:formatTime(item.startTime), //startTime 的时间为页面渲染完用户多长时间后触发的毫秒值
+            duration:formatTime(item.duration), // duration 表示该事件执行了多少时间
+            selector:lastEvent?getSelector(lastEvent.path || lastEvent.target) : ''
+          }
+          console.log("卡顿监测",log)
+        })
+      }
+    })
   }).observe({
     entryTypes:["longtask"]
   })
---
+++ src/monitor/utils/formatTime.js
@@ -0,0 +1,4 @@
+
+export default (time) => {
+  return `${time}`.split(".")[0]
+}
